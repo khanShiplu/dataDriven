f6a115ffafc45457392f27fc41c002481be231bf
unknown <khanshiplu8200@yahoo.com>
2015-12-03 01:43:04 -0500

    only java file

---
+++ src/test/java/dataDriven/Test.java
@@ -0,0 +1,14 @@
+package dataDriven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test {
+
+	public static void main(String[] args) {
+		WebDriver  driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+
+	}
+
+}
