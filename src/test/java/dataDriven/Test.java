ac4889a80499190e7e713a56368d121571a9d8d7
unknown <khanshiplu8200@yahoo.com>
2015-12-03 01:54:56 -0500

    this is my secend commit

--- src/test/java/dataDriven/Test.java
+++ src/test/java/dataDriven/Test.java
@@ -8,6 +8,7 @@ public class Test {
 	public static void main(String[] args) {
 		WebDriver  driver = new FirefoxDriver();
 		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
 
 	}
 
